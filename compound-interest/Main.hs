37fbfa23dbf819c17d340790e9201c24a56f169d
Tom Hawkins <t@groq.com>
2022-05-23 10:40:01 -0700

    Updates to interest example.

--- compound-interest/Main.hs
+++ compound-interest/Main.hs
@@ -12,45 +12,54 @@ type Mas = Double  -- The Misty May Awesomeness Scale
 type PlayerEffort = Double  -- I.e. Interest Rate
 
 
-compound :: Mas -> PlayerEffort -> [Mas]
-compound amount rate = result
- where
-  c a = a : c a' where a' = a + amount * rate
-  result = c amount
-  --result = amount : compound (amount + amount * rate) rate
+compound :: [PlayerEffort] -> [(PlayerEffort, Mas)]
+compound effort = compound' 1 effort
 
 
-playerGrowth :: [Mas]
-playerGrowth = compound 1 0.01
+compound' :: Mas -> [PlayerEffort] -> [(PlayerEffort, Mas)]
+compound' m e = case e of
+  []                          -> []
+  todaysEffort : futureEffort -> (todaysEffort, m') : compound' m' futureEffort
+    where m' = m + todaysEffort
 
 
-playerGrowth1Vs2 :: [(Mas, Mas)]
-playerGrowth1Vs2 = zip (compound 1 0.01) (compound 1 0.02)
+playerGrowth :: [(PlayerEffort, Mas)]
+playerGrowth = compound $ repeat 0.01
 
 
-playerGrowthDelayed :: [(Mas, Mas)]
-playerGrowthDelayed = zip (firstHalf <> secondHalf) committedFromBeginning
- where
-  firstHalf              = take 51 $ compound 1 0.01
-  secondHalf             = compound (last firstHalf) 0.02
-  committedFromBeginning = compound 1 0.02
+playerGrowth1Vs2 :: [((PlayerEffort, Mas), (PlayerEffort, Mas))]
+playerGrowth1Vs2 = zip (compound $ repeat 0.01) (compound $ repeat 0.02)
+
+
+playerGrowthVaried
+  :: [((PlayerEffort, Mas), (PlayerEffort, Mas), (PlayerEffort, Mas))]
+playerGrowthVaried = zip3 (compound (replicate 50 0.01 <> repeat 0.02))  -- Slacks off in the beginning.
+                          (compound (replicate 50 0.02 <> repeat 0.01))  -- Slacks off in the end.
+                          (compound $ repeat 0.02)                       -- Gives it her all.
+
+
+days :: [a] -> [(Int, a)]
+days a = take 100 $ zip [1 ..] a
+
+
+format :: (PlayerEffort, Mas) -> String
+format (e, m) = printf "mas(%1.0f%%): %1.2f" (e * 100) m
 
 
 main :: IO ()
 main = do
   putStrLn $ unlines
-    [ printf "day: %3i    mas: %1.2f" d m
-    | (d, m) <- take 101 $ zip [0 :: Int ..] playerGrowth
+    [ printf "day: %3i    %s" d (format m) | (d, m) <- days playerGrowth ]
+
+  {-
+  putStrLn $ unlines
+    [ printf "day: %3i    %s    %s" d (format m1) (format m2)
+    | (d, (m1, m2)) <- days playerGrowth1Vs2
     ]
 
-  --putStrLn $ unlines
-  --  [ printf "day: %3i    mas(%%1): %1.2f    mas(%%2): %1.2f " d m1 m2
-  --  | (d, (m1, m2)) <- take 101 $ zip [0 :: Int ..] playerGrowth1Vs2
-  --  ]
-
-  --putStrLn $ unlines
-  --  [ printf "day: %3i    mas(%%1 then %%2): %1.2f    mas(%%2): %1.2f " d m1 m2
-  --  | (d, (m1, m2)) <- take 101 $ zip [0 :: Int ..] playerGrowthDelayed
-  --  ]
-
+  putStrLn $ unlines
+    [ printf "day: %3i    %s    %s   %s" d (format m1) (format m2) (format m3)
+    | (d, (m1, m2, m3)) <- days playerGrowthVaried
+    ]
+    -}
 
