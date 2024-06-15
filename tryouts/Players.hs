278631ce8bcb3bd5fb42a3e78d22cbd4c6dc1e6d
Tom Hawkins <t@groq.com>
2024-06-15 15:42:39 -0700

    Updates to roster.

--- tryouts/Players.hs
+++ tryouts/Players.hs
@@ -1,40 +1,50 @@
+{-# LANGUAGE LambdaCase #-}
+
 module Players
   ( Player (..),
     allPlayers,
     Spot (..),
     allSpots,
+    playerName,
+    readPlayers,
+    checkPlayers,
   )
 where
 
+import Data.List (sort, (\\))
+
 data Player
   = Aaliyah
-  | Abby
-  | AddieB
-  | AddyD
+  | AbbyM
+  | AbbyW
+  | Addie
+  | Addy
   | Alisha
   | Amber
   | Anja
   | AnneKerry
   | Ari
-  | AudreyS
-  | AudrieO
+  | Audrey
+  | Audriana
   | Autumn
   | Aylie
   | BaileyB
   | BaileyU
+  | Brithany
   | Cassy
   | Chloe
   | Clea
   | Cora
   | Eden
-  | Eliza
+  | ElizaN
   | ElizaP
   | Etta
   | Faith
   | Finnley
-  | GwenH
-  | GwynS
+  | Gwen
+  | Gwyn
   | Indie
+  | Jada
   | Jayda
   | Kali
   | Kayla
@@ -53,34 +63,142 @@ data Player
   | Mary
   | Matilda
   | Merritt
+  | Mikel
   | Morgan
   | Neybi
+  | Nola
   | Nyah
   | OliviaF
   | OliviaN
   | OliviaS
   | OliviaZ
   | Ophelia
+  | Pearl
   | Prudence
-  | RileyE
+  | Ramona
+  | Riley
+  | RyleeF
   | RyleeD
-  | RyleeParks
   | Sadie
-  | Samantha
+  | Sam
   | Selina
-  | SofiaC
+  | Sofia
   | Sonoma
-  | SophiaC
+  | Sophia
   | Story
   | Tierra
   | Violeta
   | Vivian
   | Wren
-  | NolaD  -- Maybe Chloe?
   deriving (Show, Eq, Ord, Enum)
 
 allPlayers :: [Player]
 allPlayers = [Aaliyah .. Wren]
+
+playerName :: Player -> String
+playerName = \case
+  Aaliyah -> "Erickson, Aaliyah"
+  AbbyM -> "Morell, Abigail"
+  AbbyW -> "Webb, Abigail"
+  Addie -> "Bonner, Addison"
+  Addy -> "Dupre, Addyson"
+  Alisha -> "Garcia, Alisha"
+  Amber -> "Keay, Amber"
+  Anja -> "Greene, Anja"
+  AnneKerry -> "Smith, Anne-Kerry"
+  Ari -> "Weedman, Aurelia"
+  Audrey -> "Sorgen, Audrey"
+  Audriana -> "Owens, Audriana"
+  Autumn -> "Cannell, Autumn"
+  Aylie -> "Hughes, Aylie"
+  BaileyB -> "Burge, Bailey"
+  BaileyU -> "Urdiales, Bailey"
+  Brithany -> "Valerio, Brithany"
+  Cassy -> "Berwick, Cassandra"
+  Chloe -> "Coppin, Chloe"
+  Clea -> "Cristofaro, Clea"
+  Cora -> "Allen, Cora"
+  Eden -> "Moore, Eden"
+  ElizaN -> "Ragsdale, Eliza"
+  ElizaP -> "Parker, Eliza"
+  Etta -> "Williard, Etta"
+  Faith -> "Hoiland, Faith"
+  Finnley -> "Boling, Finnley"
+  Gwen -> "Hayden, Guinevere"
+  Gwyn -> "Sellinger, Gwyneth"
+  Indie -> "Shaver, Indie"
+  Jada -> "Hinderman, Jada"
+  Jayda -> "Knutsen, Jayda"
+  Kali -> "Boak, Kali"
+  Kayla -> "Morell, Kayla"
+  Lael -> "Rowerdink, Lael"
+  Laine -> "Garat, Laine"
+  Lauren -> "Johnston, Lauren"
+  LaylaM -> "Montroy, Layla"
+  LaylaW -> "Worline, Layla"
+  Lila -> "Geracie, Lila"
+  Lily -> "Jude, Lilyauna"
+  Lucy -> "Borchert, Lucy"
+  Lulu -> "Munro, Louise"
+  Maeve -> "Cochran, Maeve"
+  Malaya -> "Robles, Malaya"
+  Mariam -> "Najar, Mariam"
+  Mary -> "Lockhart, Mary"
+  Matilda -> "Mead, Matilda"
+  Merritt -> "Jensen, Merritt"
+  Mikel -> "Grove, Mikel"
+  Morgan -> "Brennan, Morgan"
+  Neybi -> "Vasquez, Neybi"
+  Nola -> "Renner, Nola"
+  Nyah -> "Lockwood, Nyah"
+  OliviaF -> "Favro, Olivia"
+  OliviaN -> "Naylor, Olivia"
+  OliviaS -> "Siemanowski, Olivia"
+  OliviaZ -> "Zawoysky, Olivia"
+  Ophelia -> "Van-Leeuwen, Ophelia"
+  Pearl -> "Hanna, Pearl"
+  Prudence -> "Van-Leeuwen, Prudence"
+  Ramona -> "Muir, Ramona"
+  Riley -> "Eaton, Riley"
+  RyleeD -> "Dwyer, Rylee"
+  RyleeF -> "Foote, Rylee"
+  Sadie -> "Hanley, Sadie"
+  Sam -> "Moore, Samantha"
+  Selina -> "Perez-Baker, Selina"
+  Sofia -> "Challa, Sofia"
+  Sonoma -> "Smith, Sonoma"
+  Sophia -> "Colina-Chacon, Sophia"
+  Story -> "Rayne, Saturn"
+  Tierra -> "Bonner, Tierra"
+  Violeta -> "Martinez-Farias, Violeta"
+  Vivian -> "Tobosa, Vivian"
+  Wren -> "Katzenberg, Wren"
+
+checkPlayers :: FilePath -> IO ()
+checkPlayers roster = do
+  p1 <- sort <$> readPlayers roster
+  let p2 = sort $ playerName <$> allPlayers
+  if p1 == p2
+    then putStrLn "All players accounted for."
+    else do
+      putStrLn $ "Players in database but not in final-forms: " <> show (p2 \\ p1)
+      putStrLn $ "Players in final-forms but not in database: " <> show (p1 \\ p2)
+
+readPlayers :: FilePath -> IO [String]
+readPlayers f = readPlayers' <$> readFile f
+
+readPlayers' :: String -> [String]
+readPlayers' c = f2 <$> tail (lines c)
+  where
+    f1 = \case
+      ',' -> ' '
+      ' ' -> '-'
+      a -> a
+    f2 l = lastName <> ", " <> firstName
+      where
+        l1 = words $ f1 <$> l
+        firstName = tail $ init $ l1 !! 2
+        lastName = tail $ init $ l1 !! 3
 
 data Spot
   = VarsitySetter
